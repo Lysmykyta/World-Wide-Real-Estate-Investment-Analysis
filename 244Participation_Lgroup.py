e5ae6b9f3742c3f1efc6d7859e55d038dd8f4b08
Lysmykyta <98468140+Lysmykyta@users.noreply.github.com>
2026-07-28 13:53:15 -0400

    Add files via upload
    Signed-off-by: Lysmykyta <98468140+Lysmykyta@users.noreply.github.com>

---
+++ 244Participation_Lgroup.py
@@ -0,0 +1,6 @@
+import math
+import pandas as pd
+import numpy as np
+tStores = pd.read_csv('/Users/student/Documents/UWaterloo/AFM 244 Projects/tStores.csv')
+tProducts = pd.read_csv('/Users/student/Documents/UWaterloo/AFM 244 Projects/tProducts (1).csv')
+tEndInv = pd.read_csv('/Users/student/Documents/UWaterloo/AFM 244 Projects/tEndInv.csv')
